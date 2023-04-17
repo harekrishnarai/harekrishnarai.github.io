6dd40bf8dabfc02966c7dd6c5d5ee19ed2cfac93
harekrishnarai <786hkr@gmail.com>
2023-04-18 03:29:39 +0530

    Updates

---
+++ precache-manifest.b09b75c7d3d953d5591844fa8cee6638.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e835b747d21fac2e1fbb220fa77883",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4313bd67e975445df5c0",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "c64dbbb121de0e78ccb9",
+    "url": "/static/css/main.3d9f80db.chunk.css"
+  },
+  {
+    "revision": "4313bd67e975445df5c0",
+    "url": "/static/js/2.b8608755.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.b8608755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c64dbbb121de0e78ccb9",
+    "url": "/static/js/main.3b79df8b.chunk.js"
+  },
+  {
+    "revision": "30970e5edd6135ec45c3",
+    "url": "/static/js/runtime-main.6abdb680.js"
+  }
+]);
